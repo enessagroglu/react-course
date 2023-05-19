5f41ef83cb3070979487a4cdd8b9493e8441c43a
enessagroglu <enes-2001sagroglu@hotmail.com>
2023-05-19 16:35:31 +0300

    update files

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const selectedExpenses = props.items.filter(expense=>{
+    return filteredYear===expense.date.getFullYear().toString()
+  })
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+
+        {selectedExpenses.map((expense) => (
+
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+        }
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
